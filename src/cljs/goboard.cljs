faa98a8abd292e3930b6d39f4185e5a3d76be71b
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-03-10 16:43:59 +0100

    init project: draw a blank board

---
+++ src/cljs/goboard.cljs
@@ -0,0 +1,32 @@
+(ns goboard.goboard)
+
+(.write js/document "<p>Hello, world!!</p>")
+
+(def ^:const board-background-color "#E8BD68")
+(def ^:const board-lines 19)
+(def ^:const board-offset 20)
+(def ^:const canvas-size 620)
+(def ^:const inner-board (* (/ 9 10) canvas-size))
+(def ^:const space-width (.floor js/Math (/ inner-board board-lines)))
+(def ^:const board-margin (* (- board-lines 1) space-width))
+
+(def ^:const board-canvas
+  (. js/document (getElementById "goBoard")))
+(def ^:const board-ctx
+  (. board-canvas (getContext "2d")))
+
+(defn setup-board [canvas size]
+  (set! (. canvas -height) size)
+  (set! (. canvas -width) size))
+
+(defn draw-background [context background-color space offset margin]
+  (set! (. context -fillStyle) background-color)
+  (let [x (- offset space)
+        y x
+        width (+ margin (* (/ 7 3) space))
+        height width]
+    (. context (fillRect x y width height))))
+
+(setup-board board-canvas canvas-size)
+(draw-background board-ctx board-background-color space-width board-offset
+                 board-margin)
