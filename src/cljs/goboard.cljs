4931911e3d1f19d8f372b6baea3e60de8220e721
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-03-11 23:50:58 +0100

    use a map to store board constants

--- src/cljs/goboard.cljs
+++ src/cljs/goboard.cljs
@@ -1,49 +1,46 @@
 (ns goboard.goboard)
 
-(def ^:const board-background-color "#E8BD68")
-(def ^:const line-color "#444")
-(def ^:const board-lines 19)
-(def ^:const board-offset 20)
-(def ^:const canvas-size 620)
-(def ^:const inner-board (* (/ 9 10) canvas-size))
-(def ^:const space-width (.floor js/Math (/ inner-board board-lines)))
-(def ^:const board-margin (* (- board-lines 1) space-width))
+(def ^:const board {:lines 19
+                    :size 620
+                    :offset 20
+                    :inner 558  ; (* (/ 9 10) canvas-size)
+                    :space 29  ; (.floor js/Math (/ inner-board board-lines))
+                    :margin 522  ; (* (- board-lines 1) space-width))
+                    :canvas (. js/document (getElementById "goBoard"))
+                    :context (. (. js/document (getElementById "goBoard"))
+                                (getContext "2d"))
+                    :background-color "#E8BD68"
+                    :line-color "#444"
+                    })
 
 ; hack to draw pixel-perfect lines
 (def ^:const pixel 0.5)
 
-(def ^:const board-canvas
-  (. js/document (getElementById "goBoard")))
-(def ^:const board-ctx
-  (. board-canvas (getContext "2d")))
+(defn setup-board [board]
+  (set! (. (board :canvas) -height) (board :size))
+  (set! (. (board :canvas) -width) (board :size)))
 
-(defn setup-board [canvas size]
-  (set! (. canvas -height) size)
-  (set! (. canvas -width) size))
-
-(defn draw-background [context background-color space offset margin]
-  (set! (. context -fillStyle) background-color)
-  (let [x (- offset space)
+(defn draw-background [board]
+  (set! (. (board :context) -fillStyle) (board :background-color))
+  (let [x (- (board :offset) (board :space))
         y x
-        width (+ margin (* (/ 7 3) space))
+        width (+ (board :margin) (* (/ 7 3) (board :space)))
         height width]
-    (. context (fillRect x y width height))))
+    (. (board :context) (fillRect x y width height))))
 
-(defn draw-lines [context lines space offset margin color]
-  (doseq [x (take lines
-                  (iterate (partial + space)
-                           (+ pixel offset)))]
+(defn draw-lines [board]
+  (doseq [x (take (board :lines)
+                  (iterate (partial + (board :space))
+                           (+ pixel (board :offset))))]
     ; horizontal lines
-    (. context (moveTo offset x))
-    (. context (lineTo (+ margin offset) x))
+    (. (board :context) (moveTo (board :offset) x))
+    (. (board :context) (lineTo (+ (board :margin) (board :offset)) x))
     ; vertical lines
-    (. context (moveTo x offset))
-    (. context (lineTo x (+ margin offset))))
-  (set! (. context -strokeStyle) color)
-  (. context (stroke)))
+    (. (board :context) (moveTo x (board :offset)))
+    (. (board :context) (lineTo x (+ (board :margin) (board :offset)))))
+  (set! (. (board :context) -strokeStyle) (board :color))
+  (. (board :context) (stroke)))
   
-(setup-board board-canvas canvas-size)
-(draw-background board-ctx board-background-color space-width board-offset
-                 board-margin)
-(draw-lines board-ctx board-lines space-width board-offset board-margin
-            line-color)
+(setup-board board)
+(draw-background board)
+(draw-lines board)
