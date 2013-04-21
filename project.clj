897660bf728bfebf5d11c9dc1d5f4389c5c267eb
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-04-21 17:04:58 +0200

    upgrade clojurescript

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-  		 [org.clojure/clojurescript "0.0-1450"]]
+  		 [org.clojure/clojurescript "0.0-1586"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :cljsbuild {:builds
               [{:source-path "src/cljs"
