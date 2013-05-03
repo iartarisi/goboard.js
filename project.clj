5b20a4a734c3d681cc74a7f11d756dce15e23ea7
Ionuț Arțăriși <mapleoin@lavabit.com>
2013-05-03 17:27:11 +0300

    add more metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject goboard "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Go Board/Goban library"
+  :url "http://github.com/mapleoin/goboard.js"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
